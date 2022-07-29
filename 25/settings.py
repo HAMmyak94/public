050d7f99dff23bb303eff8d2080c29c00e3f001e
Evgeniy Mikheev <101253787+HAMmyak94@users.noreply.github.com>
2022-07-29 16:28:49 +0300

    Add files via upload

--- 25/settings.py
+++ 25/settings.py
@@ -3,26 +3,16 @@ url = "http://petfriends.skillfactory.ru/"
 valid_email = "hammyak94@gmail.com"
 valid_password = "285993aZ"
 
-reg_name = "hammyak94"
-reg_email = "hammyak94@gmail.com"
-reg_password = "285993aZ"
-reg_screenshot_file = 'result_reg_petfriends.png'
-reg_err_screenshot_file = 'error_reg_petfriends.png'
-
-allpet_screenshot_file = 'all_petfriends.png'
-mypet_screenshot_file = 'my_petfriends.png'
-
-css_my_pets_images = 'div#all_my_pets table tbody tr th img'
-css_my_pets_names = 'tbody tr td:nth-of-type(1)'
-css_my_pets_types = 'tbody tr td:nth-of-type(2)'
-css_my_pets_ages = 'tbody tr td:nth-of-type(3)'
-css_my_pets_stat = 'div.\.col-sm-4.left'
-css_my_pets_submit = 'button[type="submit"]'
+pets_images = 'div#all_my_pets table tbody tr th img'
+pets_names = 'tbody tr td:nth-of-type(1)'
+pets_types = 'tbody tr td:nth-of-type(2)'
+pets_ages = 'tbody tr td:nth-of-type(3)'
+pets_stat = 'div.\.col-sm-4.left'
+pets_submit = 'button[type="submit"]'
 
 link_text_btn_exist_acc = u"У меня уже есть аккаунт"
-xpath_navbar_my_pets = "//*[@id = 'navbarNav']/ul[1]/li[1]/a[1]"
-xpath_all_pets_submit = "//button[@type='submit']"
-xpath_btn_newuser = "//button[@onclick=\"document.location='/new_user';\"]"
-xpath_btn_reg_newuser = "//body/div[1]/div[1]/form[1]/div[4]/button[1]"
+navbar_my_pets = "/html/body/nav/div[1]/ul/li[1]/a"
+all_pets_submit = "//button[@type='submit']"
+btn_newuser = "//button[@onclick=\"document.location='/new_user';\"]"
+btn_reg_newuser = "//body/div[1]/div[1]/form[1]/div[4]/button[1]"
 
-path_by_driver = r"C:/driver.exe"
--- 25/test.py
+++ 25/test.py
@@ -1,69 +1,40 @@
-import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import settings as s
 
 
-def test_show_my_pets(selenium):
-    # Переходим на страницу авторизации
-    pytest.driver.get(s.url + 'login')
 
-    # Устанавливаем явное ожидание появления поля 'email'
-    WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.ID, 'email')))
+def test_show_my_pets(driver):
+    driver.set_window_size(1024, 768)
+    driver.get(s.url + 'login')
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'email')))
+    driver.find_element_by_id('email').send_keys(s.valid_email)
+    driver.find_element_by_id('pass').send_keys(s.valid_password)
+    driver.find_element_by_css_selector(s.pets_submit).click()
+    driver.find_element_by_xpath(s.navbar_my_pets).click()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'all_my_pets')))
 
-    # Вводим email
-    pytest.driver.find_element_by_id('email').send_keys(s.valid_email)
-    # Вводим пароль
-    pytest.driver.find_element_by_id('pass').send_keys(s.valid_password)
-    # Нажимаем на кнопку входа в аккаунт
-    pytest.driver.find_element_by_css_selector(s.css_my_pets_submit).click()
+    pet_images = driver.find_elements_by_css_selector(s.pets_images)
+    pet_names = driver.find_elements_by_css_selector(s.pets_names)
+    pet_types = driver.find_elements_by_css_selector(s.pets_types)
+    pet_ages = driver.find_elements_by_css_selector(s.pets_ages)
+    pet_numbers = driver.find_elements_by_css_selector(s.pets_stat)[0].text.split('\n')[1].split(' ')[1]
 
-    # Устанавливаем явное ожидание появления navbar
-    WebDriverWait(pytest.driver, 10).until(EC.element_to_be_clickable((By.XPATH, s.xpath_navbar_my_pets)))
-
-    # Проверяем, что мы оказались на главной странице пользователя
-    assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
-    # Нажимаем на кнопку "Мои питомцы"
-    pytest.driver.find_element_by_xpath(s.xpath_navbar_my_pets).click()
-
-    # Устанавливаем явное ожидание появления таблицы
-    WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.ID, 'all_my_pets')))
-
-    pet_images = pytest.driver.find_elements_by_css_selector(s.css_my_pets_images)
-    pet_names = pytest.driver.find_elements_by_css_selector(s.css_my_pets_names)
-    pet_types = pytest.driver.find_elements_by_css_selector(s.css_my_pets_types)
-    pet_ages = pytest.driver.find_elements_by_css_selector(s.css_my_pets_ages)
-    pet_numbers = pytest.driver.find_elements_by_css_selector(s.css_my_pets_stat)[0].text.split('\n')[1].split(' ')[1]
-
-    # Проверяем все-ли питомцы есть в таблице
     assert int(pet_numbers) == len(pet_names)
 
     pet_this_photo = 0
     list_pet_names = []
-    list_pets = {}
     for i in range(len(pet_names)):
-        # Считаем количество питомцев с фото
         pet_this_photo += 0 if pet_images[i].get_attribute('src') == "" else 1
 
-        # Проверяем, что у всех питомцев есть имя, порода и возраст
         assert pet_names[i].text != ''
         assert pet_types[i].text != ''
         assert pet_ages[i].text != ''
 
-        # Создаем список неповторяющихся имён питомцев
         if pet_names[i].text not in list_pet_names:
             list_pet_names.append(pet_names[i].text)
 
-        # Создаем словарь неповторяющихся питомцев
-        list_pets.update({pet_names[i].text + pet_types[i].text + pet_ages[i].text: (
-            pet_names[i].text, pet_types[i].text, pet_ages[i].text)})
+        assert pet_this_photo >= (int(pet_numbers) + 1) // 2
 
-    # Проверяем, что питомцев с фото не меньше половины
-    assert pet_this_photo >= (int(pet_numbers) + 1) // 2
-
-    # Проверяем, что все имена питомцев различны (количество неповторяющихся имен = кол-ву питомцев)
-    assert len(list_pet_names) == int(pet_numbers)
-
-    # Проверяем, что в списке нет повторяющихся питомцев (кол-во в словаре = общему кол-ву питомцев)
-    assert len(list_pets) == int(pet_numbers)
+        assert len(list_pet_names) == int(pet_numbers)
